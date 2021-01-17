febcb3b8b34ed149b7cf716cfa0985dd2bf13a62
Beatriz Albuquerque Rodrigues <beatriz@lepidus.com.br>
2021-01-17 18:22:58 -0400

    Adiciona condiçao  que verifica se usuario ou email ja estao em uso no banco de dados.
    Signed-off-by: Beatriz Albuquerque Rodrigues <beatriz@lepidus.com.br>

--- registra_usuario.php
+++ registra_usuario.php
@@ -6,16 +6,58 @@
     $email = $_POST['email'];
     $senha = md5($_POST['senha']);
 
-    $objetoDb = new db();
-    $link = $objetoDb-> conecta_mysql();
+    $objDb = new db();
+    $link = $objDb->conecta_mysql();
 
-    $sql = "insert into  usuarios(usuario, email, senha) values('$usuario', ' $email', '$senha')";
+    //verificar se o usuário já existe
+    $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' ";
+    if($resultado_id = mysqli_query($link, $sql)){
 
-    //executar a Query
-    if(mysqli_query($link, $sql)){
-        echo 'Usuário registrado com sucesso';
+        $dados_usuario = mysqli_fetch_array($resultado_id);
+
+        if(isset($dados_usuario['usuario'])){
+
+            echo 'Usuário já cadastrado';
+
+        }else{
+
+            echo 'Usuário não cadastrado, ok, pode cadastrar';
+
+        }
+
+    }else{
+        echo 'Erro ao tentar localizar o registro de usuário';
     }
-    else{
-        echo 'Erro ao registrar o usuário';
-    };
+
+    //verificar se o e-mail já existe
+    $sql = "SELECT * FROM usuarios WHERE email = '$email' ";
+    if($resultado_id = mysqli_query($link, $sql)){
+
+        $dados_usuario = mysqli_fetch_array($resultado_id);
+
+        if(isset($dados_usuario['email'])){
+
+            echo 'Email já cadastrado';
+
+        }else{
+
+            echo 'Email não cadastrado, ok, pode cadastrar';
+
+        }
+
+    }else{
+        echo 'Erro ao tentar localizar o registro de email';
+    }
+
+    die();
+
+    $sql = "INSERT INTO usuarios (usuario, email, senha) VALUES ('$usuario', '$email', '$senha')";
+
+    //executar a query
+    if(mysqli_query($link, $sql)){
+        echo "Usuário registrado com sucesso!";
+    }else{
+        echo "Erro ao registrar o usuário!";
+    }
+
 ?>
